b2571f514f2d85d9f1d9de5b8e89e79be6e43465
Jorge <jtaylor34@sheffield.ac.uk>
2022-05-14 13:05:57 +0100

    Updated task2 with better values

--- src/task2.py
+++ src/task2.py
@@ -1,42 +1,38 @@
 #!/usr/bin/env python3
 
-from logging import shutdown
-from termios import VEOL
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
-from tf.transformations import euler_from_quaternion
-from math import pi
 import numpy as np
 import time
 
 class Task2:
 
     def __init__(self):
+
         node_name = "task2"
         rospy.init_node(node_name, anonymous=True)
 
         self.velocity_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         self.lidar_subscriber = rospy.Subscriber('scan', LaserScan, self.callback_lidar)
 
-        self.min = 999
-        self.argmin = 10
+        self.min = 999 # minimum distance of obstacle in-front of robot
+        self.argmin = 25 # index of minimum distance in array 
 
         self.rate = rospy.Rate(10) # hz        
         self.vel = Twist()
-
         self.ctrl_c = False
 
         rospy.on_shutdown(self.shutdownhook)
         rospy.loginfo(f"the {node_name} node has been initialised...")
 
     def callback_lidar(self, lidar_data):
-        # get the front arc of the robot
-        left_arc = lidar_data.ranges[0:30]
-        right_arc = lidar_data.ranges[-30:]
+        # get the ranges of robot front arc 
+        left_arc = lidar_data.ranges[0:25]
+        right_arc = lidar_data.ranges[-25:]
         front_arc = np.array(left_arc + right_arc)
 
-        # get the closest distance and the argmin of the closest distance
+        # get the closest distance and the index of the closest distance
         self.min = front_arc.min()
         self.argmin = front_arc.argmin()
 
@@ -44,18 +40,15 @@ class Task2:
         self.velocity_publisher.publish(Twist())
         self.ctrl_c = True
     
-    def print_lidar(self):
-        print(f"Minimum distance in-front: {self.min}")
-
     def main_loop(self):
-
+        
         start_time = time.time()
         execution_time = 0
         threshold_distance = 0.5
         moving_speed = 0.26
-        turning_speed = 1.8
+        turning_speed = 0.8
 
-        while not self.ctrl_c and execution_time < 90:
+        while not self.ctrl_c: # and execution_time < 90:
             execution_time = time.time() - start_time
             
             # if there's an obstacle in-front
@@ -67,9 +60,12 @@ class Task2:
                 else:
                     # stop the robot and turn the robot (until in-front is clear)
                     self.vel.linear.x = 0.0
-                    if start_argmin < 30:
+                    print(start_argmin)
+                    if start_argmin <= 25:
+                        # turn right
                         self.vel.angular.z = -turning_speed
                     else:
+                        # turn left
                         self.vel.angular.z = turning_speed
             else:
                 # if in-front is clear, set turning to false and move forwards again!
